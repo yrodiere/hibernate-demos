28eaadf5c8c07265bbc214150886d061a395b221
Yoann Rodière <yoann@hibernate.org>
2020-08-28 16:11:53 +0200

    Add service layer

--- hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/TShirtService.kt
+++ hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/TShirtService.kt
@@ -1,14 +1,26 @@
 package org.hibernate.search.demos.quarkuskotlin
 
+import org.hibernate.search.demos.quarkuskotlin.dto.TShirtMapper
+import org.hibernate.search.demos.quarkuskotlin.repository.TShirtRepository
+import org.jboss.resteasy.annotations.jaxrs.QueryParam
+import javax.inject.Inject
+import javax.ws.rs.Consumes
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-@Path("/hello")
-class TShirtService {
+@Path("/tshirt")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class TShirtService(
+        @Inject private val repository: TShirtRepository,
+        @Inject private val mapper: TShirtMapper
+) {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "hello"
+    @Path("/search")
+    fun search(@QueryParam terms: String, @QueryParam offset: Int = 0, @QueryParam limit: Int = 20) =
+            repository.search(terms, offset, limit)
+
 }
