a1d780b4a9f337c4234be24aadae9bd17033b0f8
Yoann Rodière <yoann@hibernate.org>
2020-08-28 16:11:53 +0200

    Add service layer

--- hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/TShirtService.kt
+++ hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/TShirtService.kt
@@ -1,14 +1,55 @@
 package org.hibernate.search.demos.quarkuskotlin
 
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import org.hibernate.search.demos.quarkuskotlin.dto.SearchResultDto
+import org.hibernate.search.demos.quarkuskotlin.dto.TShirtCreateUpdateDto
+import org.hibernate.search.demos.quarkuskotlin.dto.TShirtMapper
+import org.hibernate.search.demos.quarkuskotlin.dto.TShirtRetrieveDto
+import org.hibernate.search.demos.quarkuskotlin.orm.TShirt
+import org.hibernate.search.demos.quarkuskotlin.repository.TShirtRepository
+import org.jboss.resteasy.annotations.jaxrs.PathParam
+import org.jboss.resteasy.annotations.jaxrs.QueryParam
+import javax.inject.Inject
+import javax.transaction.Transactional
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
-@Path("/hello")
-class TShirtService {
+@Path("/tshirt")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+class TShirtService(
+        @Inject val repository: TShirtRepository,
+        @Inject val mapper: TShirtMapper
+) {
+
+    @PUT
+    @Path("/")
+    fun create(data: TShirtCreateUpdateDto): TShirtRetrieveDto {
+        val tshirt = TShirt()
+        mapper.fromDto(tshirt, data)
+        repository.persist(tshirt)
+        return mapper.toDto(tshirt)
+    }
+
+    @POST
+    @Path("/{id}/")
+    fun update(@PathParam id: Long, data: TShirtCreateUpdateDto): TShirtRetrieveDto? {
+        val tshirt = repository.findById(id) ?: throw NotFoundException()
+        mapper.fromDto(tshirt, data)
+        repository.persist(tshirt)
+        return mapper.toDto(tshirt)
+    }
+
+    @POST
+    @Path("/{id}/")
+    fun retrieve(@PathParam id: Long, data: TShirtCreateUpdateDto): TShirtRetrieveDto? {
+        val tshirt = repository.findById(id) ?: throw NotFoundException()
+        return mapper.toDto(tshirt)
+    }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "hello"
+    @Path("/search")
+    fun search(@QueryParam terms: String, @QueryParam offset: Int = 0, @QueryParam limit: Int = 20) =
+            SearchResultDto.from(repository.search(terms, offset, limit), mapper::toDto)
+
 }
