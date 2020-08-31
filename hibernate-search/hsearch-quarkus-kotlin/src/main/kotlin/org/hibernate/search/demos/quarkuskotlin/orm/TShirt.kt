e762d49deba3eb0e880551c0aacda49357f9c9db
Yoann Rodière <yoann@hibernate.org>
2020-08-31 11:05:01 +0200

    PanacheEntity

--- hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/orm/TShirt.kt
+++ hibernate-search/hsearch-quarkus-kotlin/src/main/kotlin/org/hibernate/search/demos/quarkuskotlin/orm/TShirt.kt
@@ -1,12 +1,10 @@
 package org.hibernate.search.demos.quarkuskotlin.orm
 
+import io.quarkus.hibernate.orm.panache.kotlin.PanacheEntity
 import javax.persistence.*
 
 @Entity
-open class TShirt() {
-    @Id
-    @GeneratedValue
-    var id: Long? = null
+open class TShirt : PanacheEntity() {
 
     lateinit var name: String
 
